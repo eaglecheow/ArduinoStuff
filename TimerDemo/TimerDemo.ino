e11a25e710372c92e038a805449ef953f262bb5a
Eagle Cheow <eagle.cheow@hotmail.com>
2018-03-18 10:18:21 +0800

    Updated original TimerDemo code with classes

--- TimerDemo/TimerDemo.ino
+++ TimerDemo/TimerDemo.ino
@@ -1,23 +1,44 @@
-int ledPin = 41;
+class Blink
+{
+  int LedPin;
+  float BlinkFrequency;
+  int LedState;
+  unsigned long CurrentTime;
+  unsigned long PreviousTime;
 
-long previousTime = 0;
-int ledState = LOW;
+  public:
+  Blink(int ledPin, int blinkFrequency)
+  {
+    LedPin = ledPin;
+    pinMode(LedPin, OUTPUT);
+    BlinkFrequency = blinkFrequency;
+    LedState = LOW;
+    PreviousTime = millis();
+  }
+
+  void Update()
+  {
+    CurrentTime = millis();
+    float blinkPeriod = ((float)(1/BlinkFrequency))*1000;
+    if ((CurrentTime - PreviousTime) > blinkPeriod)
+    {
+      LedState = !LedState;
+      digitalWrite(LedPin, LedState);
+      PreviousTime = CurrentTime;
+    }
+  }
+};
+
+Blink* ledOne = new Blink(31,5);
+Blink* ledTwo = new Blink(41, 50);
 
 void setup() 
 {
   Serial.begin(9600);
-  previousTime = millis();
-  pinMode(ledPin, OUTPUT);
 }
 
 void loop() 
 {
-  long currentTime = millis();
-  if ((currentTime - previousTime) > 1000)
-  {
-    ledState = !ledState;
-    digitalWrite(ledPin, ledState);
-    Serial.println((ledState == LOW)?"LED is OFF": "LED is ON");
-    previousTime = currentTime;
-  }
+  ledOne->Update();
+  ledTwo->Update();
 }
